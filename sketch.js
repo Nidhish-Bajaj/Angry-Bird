336659fcaaf9fad815f4a2d4dc12d64e874680c0
Nidhish-Bajaj <jasminechawla@gmail.com>
2020-12-11 16:46:42 +0530

    Add stage

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,stage;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,7 +18,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-
+stage=new Ground(300,400,500,200)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -54,6 +54,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
+    stage.display();
 
     box5.display();
     log4.display();
